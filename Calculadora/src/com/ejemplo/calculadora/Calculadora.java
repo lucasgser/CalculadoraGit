c9003d9948b691ea614b96464f1630a94b0ba817
Lucas García <lgarser1312@g.educaand.es>
2025-10-23 12:07:24 +0200

    Añadida operación de división con control de error

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -13,5 +13,12 @@ public class Calculadora {
 	    return a * b;
 	}
 
+	public double dividir(double a, double b) {
+	    if (b == 0) {
+	        throw new IllegalArgumentException("No se puede dividir entre cero");
+	    }
+	    return a / b;
+	}
+
 	
 }
